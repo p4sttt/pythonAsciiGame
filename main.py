e1c1c5393ede99ee6bb43e59a171b2d8ba264036
da_m1r <damirr765@gmail.com>
2022-10-24 20:09:30 +0300

    $$$

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import os
 from rich.console import Console
+from rich.progress import track
 import keyboard
 import random
+import time
 
-console = Console()
+WIDTH = 120
+HEIGHT = 30
+
+console = Console(width=WIDTH, height=HEIGHT)
 
 map = []
 
 
+def print_hello():
+    os.system('cls')
+    console.print('                     .__.__                  .__   __                 ')
+    console.print('_____    ______ ____ |__|__| __  _  _______  |  | |  | __ ___________ ')
+    console.print('\__  \  /  ___// ___\|  |  | \ \/ \/ /\__  \ |  | |  |/ // __ \_  __ \\')
+    console.print(' / __ \_\___ \\\\  \___|  |  |  \     /  / __ \|  |_|    <\  ___/|  | \/')
+    console.print('(____  /____  >\___  >__|__|   \/\_/  (____  /____/__|_ \\\___  >__|   ')
+    console.print('     \/     \/     \/                      \/          \/    \/       ')
+    print()
+    print()
+    for _ in track(range(100), description='[green]Loading...'):
+        time.sleep(0.02)
+
+
+print_hello()
+
+
 def map_generation():
-    map.append(list('1' * 120))
-    for j in range(21):
+    map.append(list('1' * WIDTH))
+    for j in range(HEIGHT - 5):
         line = [1]
-        for i in range(118):
+        for i in range(WIDTH - 2):
             if random.randint(1, 100) >= 5:
                 line.append(0)
             else:
@@ -20,7 +42,7 @@ def map_generation():
         line.append(1)
         line_str = ''.join(str(c) for c in line)
         map.append(list(line_str))
-    map.append(list('1' * 120))
+    map.append(list('1' * WIDTH))
     return map
 
 
@@ -33,22 +55,19 @@ def print_map() -> None:
             if map[i][j] == '0':
                 console.print(' ', end='')
             elif map[i][j] == '1':
-                console.print(' ', style='white on white', end='')
+                console.print('@', end='')
             elif map[i][j] == '2':
                 console.print(' ', style='white on blue', end='')
     console.print()
 
-    console.print('tap [white on blue bold]w[/white on blue bold] to move up')
-    console.print('tap [white on blue bold]a[/white on blue bold] to move left')
-    console.print('tap [white on blue bold]s[/white on blue bold] to move down')
-    console.print('tap [white on blue bold]d[/white on blue bold] to move right')
-    console.print('or tap [white on red]q[/white on red] to leave')
+    console.print('use [white on blue] w a s d [/white on blue] to move')
+    console.print('or tap [white on red] q [/white on red] to leave')
 
 
 class Player:
     def __init__(self):
-        self.position_x = random.randint(2, len(map[0])-1)
-        self.position_y = random.randint(2, len(map)-1)
+        self.position_x = random.randint(2, len(map[0])-2)
+        self.position_y = random.randint(2, len(map)-2)
         map[self.position_y][self.position_x] = '2'
         os.system('cls')
         print_map()
